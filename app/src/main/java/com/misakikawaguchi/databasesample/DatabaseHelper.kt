3b81253f019c3705d4ded690c423224dfe688042
川口美咲 <misaki@arcphilia.co.jp>
2021-02-10 22:33:52 +0900

    [Add]定数フィールド設定

--- app/src/main/java/com/misakikawaguchi/databasesample/DatabaseHelper.kt
+++ app/src/main/java/com/misakikawaguchi/databasesample/DatabaseHelper.kt
@@ -1,4 +1,18 @@
 package com.misakikawaguchi.databasesample
 
-class DatabaseHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+
+// データベースヘルパークラス
+// 第一引数：コンテキスト、第二引数：使用するデータベース名（DATABASE_NAME）
+// 第三引数：カーソルファクトリオブジェクト（通常はnull）、第四引数：データベースのバージョン番号（DATABASE_VERSION）
+class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    // クラス内のprivate定数を宣言するためにcompanion objectブロックとする
+    companion object {
+        // データベースファイル名の定数フィールド
+        private const val DATABASE_NAME = "cocktailmemo.db"
+        // バージョン情報の定数フィールド
+        private const val DATABASE_VERSION = 1
+    }
 }
