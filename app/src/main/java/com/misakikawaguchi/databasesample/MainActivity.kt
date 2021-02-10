72129966badd5e2a0d065322546b99bae62b4a1d
川口美咲 <misaki@arcphilia.co.jp>
2021-02-10 21:37:51 +0900

    [Add]カクテルの主キーと名前を表すフィールドを追加

--- app/src/main/java/com/misakikawaguchi/databasesample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/databasesample/MainActivity.kt
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // 選択されたカクテルの主キーIDを表すフィールド
+    private var _cocktailId = -1
+
+    // 選択されたカクテル名を表すフィールド
+    private var _cocktailName  = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
